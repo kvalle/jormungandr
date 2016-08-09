1427675efa337b91a4e93a537fc63a4417023561
Kjetil Valle <kjetil.valle@gmail.com>
2016-08-10 00:27:51 +0200

    GameInputs and GameWindow classes

--- orm.py
+++ orm.py
@@ -56,18 +56,6 @@ def move_snake_tail(state):
     unset_cell(state.tail, state)
     state.tail = move(state.tail, state.stack.pop(0))
 
-def draw_frame(win, state):
-    def row_to_str(row):
-        bs = bits.to_bitlist(row, length=COLS)
-        bs = map(lambda b: "o" if b==1 else " ", bs)
-        return "".join(bs)
-
-    win.border()
-    
-    for row in range(ROWS):
-        style = curses.color_pair(2) if state.running else curses.A_NORMAL
-        win.addstr(row+1, 1, row_to_str(state.snake[row]), style)
-
 def debug(stdscr, text):
     stdscr.addstr(ROWS + 5, 0, text)
     stdscr.refresh()
@@ -97,31 +85,70 @@ def detect_collision(state):
     if wall_collision or tail_collision:
         state.running = False
 
-def update_title(stdscr, state):
-    stdscr.addstr(0, 0, " jormungandr <> score: " + str(state.score))
-    if not state.running:
-        stdscr.addstr(0, COLS - 8, "GAME OVER", curses.A_BLINK)
-    stdscr.refresh()
+class GameInputs():
+    def __init__(self, stdscr):
+        self.stdscr = stdscr
+        stdscr.nodelay(True)
+
+    def get_direction(self, state):
+        key = self.stdscr.getch()
+
+        if state.stack[-1] != "right" and key == curses.KEY_LEFT:
+            return "left"
+        if state.stack[-1] != "left" and key == curses.KEY_RIGHT:
+            return "right"
+        if state.stack[-1] != "up" and key == curses.KEY_DOWN:
+            return "down"
+        if state.stack[-1] != "down" and key == curses.KEY_UP:
+            return "up"
+        else:
+            return state.direction
+
+class GameWindow():
+    def __init__(self, stdscr):
+        self.stdscr = stdscr
+
+        curses.init_pair(1, curses.COLOR_RED, curses.COLOR_BLACK)
+        curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_BLACK)
+        curses.curs_set(False)
+
+        self.win = curses.newwin(ROWS + 2, COLS + 2, 1, 0)
+        self.win.border()
+
+    def update_title(self, state):
+        self.stdscr.addstr(0, 0, " jormungandr <> score: " + str(state.score))
+        if not state.running:
+            self.stdscr.addstr(0, COLS - 8, "GAME OVER", curses.A_BLINK)
+        self.stdscr.refresh()
+
+    def draw_frame(self, state):
+        def row_to_str(row):
+            bs = bits.to_bitlist(row, length=COLS)
+            bs = map(lambda b: "o" if b==1 else " ", bs)
+            return "".join(bs)
+
+        self.win.border()
+        
+        for row in range(ROWS):
+            style = curses.color_pair(2) if state.running else curses.A_NORMAL
+            self.win.addstr(row+1, 1, row_to_str(state.snake[row]), style)
+
+        self.win.refresh()
+
 
 def main(stdscr):
     state = GameState()
+    window = GameWindow(stdscr)
+    inputs = GameInputs(stdscr)
 
-    curses.init_pair(1, curses.COLOR_RED, curses.COLOR_BLACK)
-    curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_BLACK)
-    curses.curs_set(False)
-    stdscr.nodelay(True)
-    update_title(stdscr, state)
-    
-    win = curses.newwin(ROWS + 2, COLS + 2, 1, 0)
-    win.border()
+    window.update_title(state)
     
     while True:
         # wait for next frame
         time.sleep(0.1)
 
         # inputs
-        key = stdscr.getch()
-        state.direction = get_direction(key, state)
+        state.direction = inputs.get_direction(state)
 
         # update game
         detect_collision(state)
@@ -131,10 +158,9 @@ def main(stdscr):
             move_snake_tail(state)
         
         # outputs
-        update_title(stdscr, state)
-        draw_frame(win, state)
-        win.refresh()
-        
+        window.update_title(state)
+        window.draw_frame(state)
+
 
 if __name__ == '__main__':
     try:
